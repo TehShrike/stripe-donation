cfa5549c387208c3d6c66c45fc286526568d6c31
Josh Duff <me@JoshDuff.com>
2017-04-10 14:49:03 -0500

    Dropping the server.js file

--- server.js
+++
@@ -1,8 +0,0 @@
-require('./server/server-index')({
-	stripeSecretKey: process.env.STRIPE_API_KEY,
-	stripePlanId: process.env.STRIPE_PLAN_ID || 'test-generic-donation',
-	subscriptionDescription: 'Biblical Blueprints donation',
-	bodyStyle: `
-		background-color: #faf7ee;
-`
-}).listen(process.env.PORT || 8888)
