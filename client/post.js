b0ac13e010a724d2a787d5e5a3fedde382d44fa6
Josh Duff <me@JoshDuff.com>
2017-04-11 18:07:35 -0500

    Properly interpret error statuses as errors

--- client/post.js
+++ client/post.js
@@ -1,11 +1,24 @@
 export default function post(url, data) {
 	return new Promise((resolve, reject) => {
 		const request = new XMLHttpRequest()
-		request.addEventListener('load', () => resolve(JSON.parse(request.responseText)))
+		request.addEventListener('load', handleResult)
 		request.addEventListener('error', reject)
 		request.addEventListener('abort', reject)
 		request.open('POST', url)
 		request.setRequestHeader('Content-Type', 'application/json')
 		request.send(JSON.stringify(data))
+
+		function handleResult() {
+			try {
+				const response = JSON.parse(request.responseText)
+				success(request) ?
+					resolve(response) :
+					reject(response)
+			} catch (e) {
+				reject('Invalid response: ' + request.responseText)
+			}
+		}
 	})
 }
+
+const success = request => request.status >= 200 && request.status < 400
